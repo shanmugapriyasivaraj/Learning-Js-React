c34e45034c1d91c7a97c35e2120738d62e37514b
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-04-04 13:21:12 +0530

    Object

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// VARIABLES
+// VARIABLES ---
 
 // Var -- Global Scope/Function Scope
 // Let  -- Block Scope
@@ -6,31 +6,55 @@
 
 // var
 
-function printNum() {
-  for (var i = 0; i <= 3; i++) {
-    console.log(i);
-  }
-  console.log(i);
-}
-printNum();
+// function printNum() {
+//   for (var i = 0; i <= 3; i++) {
+//     console.log(i);
+//   }
+//   console.log(i);
+// }
+// printNum();
 
 // Let
 
-function printNum1() {
-  for (let i = 0; i <= 3; i++) {
-    console.log(i);
-  }
-  //   console.log(i);
-}
-printNum1();
+// function printNum1() {
+//   for (let i = 0; i <= 3; i++) {
+//     console.log(i);
+//   }
+//   //   console.log(i);
+// }
+// printNum1();
 
 // Const
 
-function printNumber() {
-  if (true) {
-    const x = 10;
-  }
-  const x = 10;
-  x = 15;
-}
-printNumber();
+// function printNumber() {
+//   if (true) {
+//     const x = 10;
+//   }
+//   const x = 10;
+//   x = 15;
+// }
+// printNumber();
+
+// OBJECTS---
+
+const person = {
+  firstName: "Guvi",
+  lastName: "Geeks",
+  getName() {
+    console.log(this);
+  },
+};
+const person2 = {
+  firstName: "Guvi2",
+  lastName: "Geeks2",
+};
+console.log(person.firstName); //[Dot Operator]
+
+const key = "lastName";
+
+console.log(person[key]);
+
+person.getName();
+
+const name = person.getName.bind(person2);
+name();
